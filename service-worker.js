0714bb0dfd55b39b279a038a68ede80a48029f67
brand1345 <byuan45@gatech.edu>
2020-08-19 16:07:11 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aa00f70b981ba6cdbda66a6147ec3627.js"
+  "/portfolio/precache-manifest.29cd6f736e22756af2b17e37934e5077.js"
 );
 
 self.addEventListener('message', (event) => {
