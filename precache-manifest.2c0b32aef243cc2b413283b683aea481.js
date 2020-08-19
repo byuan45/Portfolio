77116d43adf67eb04564b29427affdc5863f9760
brand1345 <byuan45@gatech.edu>
2020-08-19 14:59:10 -0400

    Updates

---
+++ precache-manifest.2c0b32aef243cc2b413283b683aea481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248cdd0b7e4038311192d6ff8269b992",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1ebfa450d0dabd6a362",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "24998e1895e098f41664",
+    "url": "/portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d1ebfa450d0dabd6a362",
+    "url": "/portfolio/static/js/2.b8242629.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.b8242629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24998e1895e098f41664",
+    "url": "/portfolio/static/js/main.a71745e2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.24a21fbb9d0b1cee1f6e2503a90d1824.js"
+  "/portfolio/precache-manifest.2c0b32aef243cc2b413283b683aea481.js"
 );
 
 self.addEventListener('message', (event) => {
