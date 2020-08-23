6e6dee62bffc5c889a4a486ce0dded422cd8cb5b
brand1345 <byuan45@gatech.edu>
2020-08-23 16:14:20 -0400

    added in pics and tab+carousel

--- src/components/travel.js
+++ src/components/travel.js
@@ -1,18 +1,31 @@
 import React, { memo } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { Carousel } from "react-bootstrap";
-import First from "../travelImages/Antarctica/first.jpg";
-import Second from "../travelImages/Antarctica/second.jpg";
-import Third from "../travelImages/Antarctica/third.jpg";
+import { Carousel, Tabs, Tab } from "react-bootstrap";
+import AntFirst from "../travelImages/Antarctica/first.jpg";
+import AntSecond from "../travelImages/Antarctica/second.JPG";
+import AntThird from "../travelImages/Antarctica/third.jpg";
+import AntForth from "../travelImages/Antarctica/forth.jpg";
+import AntFifth from "../travelImages/Antarctica/fifth.jpg";
+import AntSixth from "../travelImages/Antarctica/sixth.jpg";
+import CosFirst from "../travelImages/Costa Rica/first.jpg";
+import CosSecond from "../travelImages/Costa Rica/second.jpg";
+import CosThird from "../travelImages/Costa Rica/third.jpg";
+import CosForth from "../travelImages/Costa Rica/forth.jpg";
+import ChiFirst from "../travelImages/Chile/first.jpg";
+import ChiSecond from "../travelImages/Chile/second.jpg";
+import ChiThird from "../travelImages/Chile/third.jpg";
+import ChiForth from "../travelImages/Chile/forth.jpg";
+import ChiFifth from "../travelImages/Chile/fifth.jpg";
+
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const highlighted = ["LBR", "RUS"];
+const highlighted = ["USA", "ATA", "CHL", "CRI", "MYS"];
 const Travel = ({ setTooltipContent }) => {
   return (
     <div>
-      <h1>Intresting Title</h1>
+      <h1>Travel</h1>
       <p>Random paragraph</p>
 
       <ComposableMap
@@ -58,36 +71,130 @@ const Travel = ({ setTooltipContent }) => {
           }
         </Geographies>
       </ComposableMap>
-
-      <Carousel
-       style={{ height: "500px", width: "500px", margin: 0 }}
+      <Tabs
+        defaultActiveKey="Antarctica"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          padding: "10px",
+        }}
       >
-        <Carousel.Item>
-          <img className="d-block w-100" src={First} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Second} alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Third} alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+        <Tab eventKey="Antarctica" title="Antarctica">
+          <Carousel
+            style={{
+              paddingLeft: "100px",
+              paddingRight: "100px",
+              margin: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <Carousel.Item>
+              <img className="d-block w-100" src={AntFirst} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={AntSecond}
+                alt="Second slide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={AntThird} alt="Third slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={AntForth} alt="Forth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={AntFifth} alt="Fifth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={AntSixth} alt="Sixth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Tab>
+        <Tab eventKey="Alaska" title="Alaska"></Tab>
+        <Tab eventKey="Chile" title="Chile">
+        <Carousel
+            style={{
+              paddingLeft: "100px",
+              paddingRight: "100px",
+              margin: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <Carousel.Item>
+              <img className="d-block w-100" src={ChiFirst} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={ChiSecond}
+                alt="Second slide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={ChiThird} alt="Third slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={ChiForth} alt="Forth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={ChiFifth} alt="Fifth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Tab>
+        <Tab eventKey="Costa Rica" title="Costa Rica">
+          <Carousel
+            style={{
+              paddingLeft: "100px",
+              paddingRight: "100px",
+              margin: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <Carousel.Item>
+              <img className="d-block w-100" src={CosFirst} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={CosSecond}
+                alt="Second slide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={CosThird} alt="Third slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={CosForth} alt="Forth slide" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Tab>
+        <Tab eventKey="Singapore" title="Singapore"></Tab>
+      </Tabs>
     </div>
   );
 };
